f032eb0da2fd67f14780091ee73db95624b70751
DipeshPTemp <dipeshptemp@Dipesh-Patils-MacBook-Pro.local>
2022-09-19 23:01:00 +0530

    Frontend: Add: Transaction Component

--- client/screens/HomeScreen.js
+++ client/screens/HomeScreen.js
@@ -1,6 +1,10 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, SafeAreaView, ScrollView } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
+
+import Transactions from "../components/Transactions";
+import TransactionSummary from "../components/TransactionSummary";
+import transactionData from "../data/transactions.json";
 
 export default function HomeScreen() {
   const navigation = useNavigation();
@@ -12,10 +16,29 @@ export default function HomeScreen() {
   }, []);
 
   return (
-    <SafeAreaView className="bg-white pt-5">
-      <View className="flex-1 items-center justify-center bg-white">
-        <Text>Home Screen</Text>
+    <SafeAreaView>
+      <View className="flex-row items-center mx-2 space-x-2">
+        <View className="flex-1">
+          <Text className="font-bold p-1 text-xl">Summary</Text>
+        </View>
       </View>
+
+      <TransactionSummary data={transactionData} />
+
+      <View className="flex-row items-center mx-2 space-x-2">
+        <View className="flex-1">
+          <Text className="font-bold p-1 text-xl">Transactions</Text>
+        </View>
+      </View>
+
+      <ScrollView
+        className="bg-gray-100"
+        contentContainerStyle={{
+          paddingBottom: 100,
+        }}
+      >
+        <Transactions />
+      </ScrollView>
     </SafeAreaView>
   );
 }
