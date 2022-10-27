c366137ddcfc85cc40968fe6d2c07cd8f32093a8
Dipesh Patil <dipeshpatil@Ds-MacBook-Pro.local>
2022-10-27 13:29:59 +0530

    Added Strings in Config

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,16 +1,18 @@
 const jwt = require("jsonwebtoken");
 const config = require("config");
 
+const { NO_AUTH_TOKEN_MESSAGE, INVALID_TOKEN } = config.get("strings");
+
 module.exports = function (req, res, next) {
   const token = req.header("x-auth-token");
   if (!token) {
-    return res.status(401).json({ message: "No Token, Authorization Denied!" });
+    return res.status(401).json({ message: NO_AUTH_TOKEN_MESSAGE });
   }
   try {
     const decoded = jwt.verify(token, config.get("jwtSecret"));
     req.user = decoded.user;
     next();
   } catch (err) {
-    res.status(401).json({ message: `Invalid Token! ${err.message}` });
+    res.status(401).json({ message: `${INVALID_TOKEN} ${err.message}` });
   }
 };
